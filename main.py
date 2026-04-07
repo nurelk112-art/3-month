6d3bcff7fd0a2e745d8a9081878c0a8be87a0c1e
nurelk112-art <nurelk112gmail.com>
2026-04-07 20:05:30 +0600

    new

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Hello world")
+import flet as ft
+
+
+def main_page(page: ft.Page):
+    page.title = 'Мое первое приложение'
+    page.theme_mode = ft.ThemeMode.LIGHT
+
+    text_hello = ft.Text(value='Hello world')
+
+    text_hello.value = 'Hello Geeks'
+    # text_hello = 'Hello world'
+
+    name_input = ft.TextField()
+
+    def on_button_click(_):
+        print(name_input.value)
+    
+    elavated_button = ft.ElevatedButton('send', icon=ft.Icons.SEND, color=ft.Colors.RED, 
+                                        icon_color=ft.Colors.GREEN, on_click=on_button_click)
+
+
+
+    page.add(text_hello, name_input, elavated_button)
+
+
+
+ft.app(main_page)
+# ft.app(main_page, view=ft.AppView.WEB_BROWSER)
